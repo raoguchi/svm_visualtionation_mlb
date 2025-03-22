69136b175c183de851b5ba8c436dab50dd3ea36d
rsm-roguchi <roguchi@ucsd.edu>
2025-03-22 14:03:39 -0700

    upodate

--- src/svelte.config.js
+++ src/svelte.config.js
@@ -1,7 +1,8 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import {defineConfig} from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-}
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+  base: "https://raoguchi.github.io/svm_visualtionation_mlb/"
+})
