3c5c72d92076ae11b749b3017578e572a94310ee
rsm-roguchi <roguchi@ucsd.edu>
2025-03-22 17:01:56 -0700

    update

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import App from './App.svelte';
-import './app.css'; // or './global.css'
-
-
+import './app.css';  // Ensure your CSS file is included
 
 const app = new App({
-  target: document.getElementById('app')
+  target: document.getElementById('app'),  // This should mount your app to the div with id="app"
 });
 
 export default app;
